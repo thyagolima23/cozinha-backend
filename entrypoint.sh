57d2dc546a94fd7f1c986af8aa931c447ca8c30c
thyagolima23 <thyago.lima2324@gmail.com>
2025-06-23 20:04:17 -0300

    ponto de entrada

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Running Prisma generate..."
+npx prisma generate
+
+echo "Running Prisma migrate deploy..."
+npx prisma migrate deploy
+
+echo "Running Prisma seed..."
+npx prisma db seed
+
+echo "Starting server..."
+exec node dist/index.js
